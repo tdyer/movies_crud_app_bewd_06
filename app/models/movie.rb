0941bed12e4898931b66a868970c76f4d52a6a5e
Tom Dyer <tdyer1@gmail.com>
2016-03-14 19:34:51 -0400

    Added movie migration and model, added seed data

---
+++ app/models/movie.rb
@@ -0,0 +1,4 @@
+# Subclass of ActiveRecord. So, it has superpower
+# in relation to the DB.
+class Movie < ActiveRecord::Base
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Delete existing movies in the DB
+Movie.delete_all  # ActiveRecord method to delete all rows.
+
+# Create three movies
+Movie.create!(name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123)
+# create! is a ActiveRecord method to create a DB table row and a instance of the Movie
+# The bang, !, just means STOP the running code if there is an error.
+Movie.create!(name: 'Mad Max', rating: 'R', desc: 'Fun', length: 154)
+Movie.create!(name: 'Rushmore', rating: 'PG-13', desc: 'Quirky', length: 105)
+puts "Created 3 movies"
