33e6c625da128e8593027b6829149e42d478e579
Tom Dyer <tdyer1@gmail.com>
2016-03-23 19:51:17 -0400

    Added validations for the Movie name attribute.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,4 +18,15 @@ class Movie < ActiveRecord::Base
   # Class Constant variable that is an Array of valid ratings
   # Valid Movie Ratings
   RATINGS = ['G', 'PG', 'PG-13', 'R', 'NC-17']
+
+  # validate that the movie has a name.
+  # ActiveRecord gives us this validates method.
+  # It's first argument is the name of the attribute to validate
+  # The next argument is the "type" of validation.
+  validates :name, presence: true
+  # name must be between 2 and 30 characters
+  validates :name, length: {minimum: 2, maximum: 30}
+  # name must be unique
+  validates :name, uniqueness: {scope: :released_year}
+
 end
