85ccdbc7a0b8b2e4e6e235fe31fcf89e41247995
Tom Dyer <tdyer1@gmail.com>
2016-04-04 20:11:59 -0400

    Added nested resource for movie review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,43 @@ class ReviewsController < ApplicationController
     @reviews = @movie.reviews
   end
 
+  # GET /movies/:movie_id/reviews/:id
+  def show
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  # GET /movies/:movie_id/review/new
+  def new
+
+    # We need an empty review object for the
+    # form we are going to display to the user.
+    @review = @movie.reviews.new
+  end
+
+  # POST /movies/:movie_id/reviews
+  def create
+
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      # Success, we've saved the review
+      redirect_to movie_reviews_path(@movie), notice: 'Created Review'
+    else
+      # Oops, failed to create the review
+      render :new
+    end
+  end
+
   private
 
   def set_movie
     # pull the movie id out of the URL
     # /movies/:movie_id/reviews
     @movie = Movie.find(params[:movie_id])
+  end
+
+  # Strong params for a review
+  def review_params
+    params.require(:review).permit(:content, :email)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ActiveRecord::Base
   # The Review has a foreign key to a movie
   # belongs_to is a ActiveRecord method.
   belongs_to :movie
+
+  validates :content, presence: true
+  # Validate using a Regular Expression, validation will ONLY occur when we create a review.
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
